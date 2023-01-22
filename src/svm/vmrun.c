86b97dba25d30f76832cfe05b73c4f86d84d7068
JasperGeer <jasper.geer@gmail.com>
2023-01-22 13:00:11 -0500

    fix bugs and formatting

--- src/svm/vmrun.c
+++ src/svm/vmrun.c
@@ -26,94 +26,87 @@
 #include "vmstring.h"
 #include "string.h"
 
-static inline Value add(VMState vm, Value a, Value b) {
-    return mkNumberValue(AS_NUMBER(vm, a) + AS_NUMBER(vm, b));
-}
+// static inline Value add(VMState vm, Value a, Value b) {
+//     return mkNumberValue(AS_NUMBER(vm, a) + AS_NUMBER(vm, b));
+// }
 
 
-void vmrun(VMState vm, struct VMFunction *fun) {
-    // instructon stream not used now
-    // load state into stack
-    // do we want to cache registers?
-    // Value registers[NUM_REGISTERS];
-    // memcpy(registers, vm->registers, NUM_REGISTERS * sizeof(Value));
-    uint32_t *pc = fun->instructions;
-    Value *registers = vm->registers;  // reduces one level of indirection
-    Value *literals = vm->literals;
-    // Value *globals = vm->globals;
+void vmrun(VMState vm, struct VMFunction* fun) {
+  // instructon stream not used now
+  // load state into stack
+  // do we want to cache registers?
+  // Value registers[NUM_REGISTERS];
+  // memcpy(registers, vm->registers, NUM_REGISTERS * sizeof(Value));
+  uint32_t* pc = fun->instructions;
+  Value* registers = vm->registers;  // reduces one level of indirection
+  Value* literals = vm->literals;
+  // Value *globals = vm->globals;
 
-    while (1) {
-        uint32_t curr_inst = *pc;
-        switch(opcode(curr_inst)) {
-            default:
-                print("opcode %d not implemented\n", opcode(curr_inst));
-                break;
-            case Halt:
-                vm->pc = pc;
-                vm->code = fun->instructions;
-                return;
-            case Print:
-                print("%v\n", vm->registers[uX(curr_inst)]);
-                break;
-            case Check:
-                check(vm, AS_CSTRING(vm, literals[uYZ(curr_inst)]), 
-                                         registers[uX(curr_inst)]);
-                break;
-            case Expect:
-                expect(vm, AS_CSTRING(vm, literals[uYZ(curr_inst)]),
-                                          registers[uX(curr_inst)]);
-                break;
-            case Add:
-                // add the value in uY and uZ and put it in uX
-                // project t
-                registers[uX(curr_inst)] =
-                    add(vm, registers[uY(curr_inst)], 
-                            registers[uZ(curr_inst)]);
-                break;
-            case SetZero:
-                // set the value in uX to 0
-                registers[uX(curr_inst)] = mkNumberValue(0);
-                break;
-            case GetTruth:
-                // put the truthiness of the value in uY in uX
-                registers[ux(curr_inst)] = mkBooleanValue(GET_TRUTH(vm, registers[uY(curr_inst)]));
-                break;
-            case Not:
-                registers[uX(curr_inst)] =
-                    mkBooleanValue(!AS_BOOLEAN(vm, registers[uY(curr_inst)]));
-                break;
-            case Add:
-              (vm -> registers [uX(curr_inst)]).n =
-                ((vm -> registers[uY(curr_inst)]).n 
-                  + (vm -> registers[uZ(curr_inst)]).n) ;
-              break;
-            case Sub:
-              (vm -> registers [uX(curr_inst)]).n =
-                ((vm -> registers[uY(curr_inst)]).n 
-                  - (vm -> registers[uZ(curr_inst)]).n) ;
-              break;
-            case Mul:
-              (vm -> registers [uX(curr_inst)]).n =
-                ((vm -> registers[uY(curr_inst)]).n 
-                  * (vm -> registers[uZ(curr_inst)]).n) ;
-              break;
-            case Div:
-              (vm -> registers [uX(curr_inst)]).n =
-               ((vm -> registers[uY(curr_inst)]).n 
-                  / (vm -> registers[uZ(curr_inst)]).n);
-              break;
-            case And:
-              (vm -> registers [uX(curr_inst)]).b =
-                (vm -> registers[uY(curr_inst)]).b 
-                  && (vm -> registers[uZ(curr_inst)]).b;
-              break;
-            case Or:
-              (vm -> registers [uX(curr_inst)]).b =
-                (vm -> registers[uY(curr_inst)]).b 
-                  || (vm -> registers[uZ(curr_inst)]).b;
-              break;
-        }
-        ++pc; // advance the program counter
+  while (1) {
+    uint32_t curr_inst = *pc;
+    switch (opcode(curr_inst)) {
+    default:
+      print("opcode %d not implemented\n", opcode(curr_inst));
+      break;
+    case Halt:
+      vm->pc = pc;
+      vm->code = fun->instructions;
+      return;
+    case Print:
+      print("%v\n", vm->registers[uX(curr_inst)]);
+      break;
+    case Check:
+      check(vm, AS_CSTRING(vm, literals[uYZ(curr_inst)]),
+        registers[uX(curr_inst)]);
+      break;
+    case Expect:
+      expect(vm, AS_CSTRING(vm, literals[uYZ(curr_inst)]),
+        registers[uX(curr_inst)]);
+      break;
+    case SetZero:
+      // set the value in uX to 0
+      registers[uX(curr_inst)] = mkNumberValue(0);
+      break;
+    case GetTruth:
+      // put the truthiness of the value in uY in uX
+      registers[uX(curr_inst)] = mkBooleanValue(GET_TRUTH(vm, registers[uY(curr_inst)]));
+      break;
+    case Not:
+      registers[uX(curr_inst)] =
+        mkBooleanValue(!AS_BOOLEAN(vm, registers[uY(curr_inst)]));
+      break;
+    case Add:
+      (vm->registers[uX(curr_inst)]).n =
+        ((vm->registers[uY(curr_inst)]).n
+          + (vm->registers[uZ(curr_inst)]).n);
+      break;
+    case Sub:
+      (vm->registers[uX(curr_inst)]).n =
+        ((vm->registers[uY(curr_inst)]).n
+          - (vm->registers[uZ(curr_inst)]).n);
+      break;
+    case Mul:
+      (vm->registers[uX(curr_inst)]).n =
+        ((vm->registers[uY(curr_inst)]).n
+          * (vm->registers[uZ(curr_inst)]).n);
+      break;
+    case Div:
+      (vm->registers[uX(curr_inst)]).n =
+        ((vm->registers[uY(curr_inst)]).n
+          / (vm->registers[uZ(curr_inst)]).n);
+      break;
+    case And:
+      (vm->registers[uX(curr_inst)]).b =
+        (vm->registers[uY(curr_inst)]).b
+        && (vm->registers[uZ(curr_inst)]).b;
+      break;
+    case Or:
+      (vm->registers[uX(curr_inst)]).b =
+        (vm->registers[uY(curr_inst)]).b
+        || (vm->registers[uZ(curr_inst)]).b;
+      break;
     }
+    ++pc; // advance the program counter
+  }
   return;
 }
--- src/svm/vmstate.c
+++ src/svm/vmstate.c
@@ -13,61 +13,59 @@
 #include "vmstate.h"
 #include "value.h"
 
-void freestatep(VMState *sp) {
-    assert(sp && *sp);
-    VMState vm = *sp;
-    free(vm->registers);
-    free(vm->literals);
-    free(vm);
-    //(void)vm; // suppress compiler warnings
-    //assert(0); // must free all memory associated with `vm`
+void freestatep(VMState* sp) {
+  assert(sp && *sp);
+  VMState vm = *sp;
+  free(vm->literals);
+  free(vm);
+  //(void)vm; // suppress compiler warnings
+  //assert(0); // must free all memory associated with `vm`
 }
 
 VMState newstate(void) {
-    // allocate, initialize, and return a new state
-    VMState vm = malloc(sizeof(struct VMState));
-    vm -> registers = calloc(256, sizeof(struct Value));
-    vm -> literals = calloc(LITERAL_INIT_LENGTH, sizeof(struct Value));
-    vm -> curLiteralSize = 0;
-    vm -> maxLiteralSize = LITERAL_INIT_LENGTH;
-    vm -> code = NULL;
-    vm -> pc = NULL;
-    //assert(0);
-    return vm;
+  // allocate, initialize, and return a new state
+  VMState vm = malloc(sizeof(struct VMState));
+  vm->literals = calloc(LITERAL_INIT_LENGTH, sizeof(struct Value));
+  vm->curLiteralSize = 0;
+  vm->maxLiteralSize = LITERAL_INIT_LENGTH;
+  vm->code = NULL;
+  vm->pc = NULL;
+  //assert(0);
+  return vm;
 }
 
 int literal_slot(VMState state, Value literal) {
-    // (void)state; // suppress compiler warnings
-    // (void)literal;
-    // Return a slot containing the literal, updating literal pool if needed.
-    // For module 1, you can get away with putting the literal in slot 0
-    // and returning 0.  For module 2, you'll need something slightly
-    // more sophisticated.
-    if(state -> curLiteralSize == 0) {
-      state -> curLiteralSize += 1;
-      state->literals[0] = literal;
-      return 0; 
-    }
-    if(state -> curLiteralSize == 
-        (state -> maxLiteralSize - 1)) {
-          state -> maxLiteralSize *= 2;
-          state -> literals = realloc(state -> literals, 
-                                  state->maxLiteralSize * sizeof(struct Value));
-    }
-    state -> curLiteralSize += 1;
-    state -> literals[state -> curLiteralSize - 1] = literal;
-    return state -> curLiteralSize - 1;
-    // assert(0);
+  // (void)state; // suppress compiler warnings
+  // (void)literal;
+  // Return a slot containing the literal, updating literal pool if needed.
+  // For module 1, you can get away with putting the literal in slot 0
+  // and returning 0.  For module 2, you'll need something slightly
+  // more sophisticated.
+  if (state->curLiteralSize == 0) {
+    state->curLiteralSize += 1;
+    state->literals[0] = literal;
+    return 0;
+  }
+  if (state->curLiteralSize ==
+    (state->maxLiteralSize - 1)) {
+    state->maxLiteralSize *= 2;
+    state->literals = realloc(state->literals,
+      state->maxLiteralSize * sizeof(struct Value));
+  }
+  state->curLiteralSize += 1;
+  state->literals[state->curLiteralSize - 1] = literal;
+  return state->curLiteralSize - 1;
+  // assert(0);
 }
 
 // these are for module 2 and beyond
 
 Value literal_value(VMState state, unsigned index) {
-  (void) state; (void) index; // replace with real code
+  (void)state; (void)index; // replace with real code
   assert(0);
 }
 
 int literal_count(VMState state) {
-  (void) state; // replace with real code
+  (void)state; // replace with real code
   assert(0);
 }
