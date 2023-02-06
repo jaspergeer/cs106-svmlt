66dcd0539aa0abd201a5e8bdcb311dcd60a15299
JasperGeer <jasper.geer@gmail.com>
2023-02-06 18:11:03 -0500

    divide ASMParse module

--- src/huft/src/ASMParse.hs
+++ src/huft/src/ASMParse.hs
@@ -1,48 +1,58 @@
 module ASMParse where
+import Control.Monad
 import qualified ASM as A
 import qualified ObjectCode as O
 import Text.Parsec.ByteString (Parser)
 import Data.List (intercalate)
-import Text.Parsec ( digit, many1, (<|>), string, letter, alphaNum, char)
+import Text.Parsec ( digit, many1, (<|>), string, letter, alphaNum, char, option, oneOf, spaces)
 import qualified Text.Parsec.Token as T
-import Text.Parsec.Language (emptyDef)
 
 -- parsing
+
+lexeme :: Parser a -> Parser a
+lexeme p = p <* spaces
 
 natural :: Parser Integer
 natural = read <$> many1 digit
 
 integer :: Parser Integer
-integer = negate <$ string "-" <*> natural
+integer = negate <$ char '-' <*> natural
         <|> natural
 
+double :: Parser Double
+double = read <$> ((++) <$> whole <*> decimal)
+      <|> fromIntegral <$> integer
+      where whole = (++) <$> option "" (string "-") <*> many1 digit
+            decimal = (++) <$> string "." <*> many1 digit
+
 literal :: Parser O.Literal
-literal = O.Int <$> integer
+literal = lexeme $
+        O.Int <$> integer
+        <|> O.Real <$> double
         <|> O.Bool True <$ string "true"
         <|> O.Bool False <$ string "false"
         <|> O.EmptyList <$ string "emptylist"
         <|> O.Nil <$ string "nil"
 
--- unparsing
+regs :: O.Operator -> [O.Reg] -> A.Instr
+regs op operands = A.ObjectCode (O.Regs op operands)
 
-reg :: Show a => a -> [Char]
-reg r = "r" ++ show r
+opcode :: Parser String
+opcode = lexeme $ many1 alphaNum
 
-concatSp :: [[Char]] -> [Char]
-concatSp = intercalate " "
+reg :: Parser Int
+reg = lexeme $ read <$> (char 'r' *> many1 digit)
 
-unparse1 :: A.Instr -> String
-unparse1 i = case i of
-  A.ObjectCode (O.Regs op regs) -> concatSp $ op : map reg regs
-  A.DefLabel label -> concatSp ["def", label]
-  A.GotoLabel label -> concatSp ["goto", label]
-  A.IfGotoLabel r label -> concatSp ["if", reg r, "goto", label]
-  _ -> error "unkown assembly instruction"
+eR0 op = regs op []
+eR1 op r1 = regs op [r1]
+eR2 op r1 r2 = regs op [r1, r2]
+eR3 op r1 r2 r3 = regs op [r1, r2, r3]
+eR1LIT op r1 lit = A.ObjectCode (O.RegsLit op [r1] lit)
 
-unparse :: [A.Instr] -> [String]
-unparse (i:is) =
-  case i of
-    A.LoadFunc r arity body -> concat [".loadfunc" : (unparse body),
-                                       ".endload" : (unparse is)]
-    _ -> unparse1 i : unparse is
-unparse [] = []
+singleLineInstr :: Parser A.Instr
+singleLineInstr = eR0 <$> opcode
+                <|> eR1 <$> opcode <*> reg 
+                <|> eR2 <$> opcode <*> reg <*> reg
+                <|> eR3 <$> opcode <*> reg <*> reg <*> reg
+                <|> eR1LIT <$> opcode <*> reg <*> literal
+                
