e6dc87f1cb8a8b74065a7db4168f9ee5a5c857b6
Jasper Geer <jasper.geer@gmail.com>
2023-02-07 13:16:16 -0500

    fix assembly parser

--- src/huft/src/ASMParse.hs
+++ src/huft/src/ASMParse.hs
@@ -4,8 +4,10 @@ import qualified ASM as A
 import qualified ObjectCode as O
 import Text.Parsec.String ( Parser )
 import Data.List (intercalate)
-import Text.Parsec ( digit, many1, (<|>), string, letter, alphaNum, char, option, oneOf, spaces, many, manyTill, space, newline)
+import Text.Parsec ( digit, many1, (<|>), string, letter, alphaNum, char, option, oneOf, spaces, many, manyTill, space, newline, choice, parserFail, (<?>), parserZero, try)
 import qualified Text.Parsec.Token as T
+import qualified Data.Map as M
+import Text.Parsec.Error (newErrorMessage, Message (Expect))
 
 -- parsing
 
@@ -50,19 +52,52 @@ eR2 op r1 r2 = regs op [r1, r2]
 eR3 op r1 r2 r3 = regs op [r1, r2, r3]
 eR1LIT op r1 lit = A.ObjectCode (O.RegsLit op [r1] lit)
 
+type Short = String
+
+binopTableInit :: [(Short, O.Operator)]
+binopTableInit = 
+  [("+", "add")
+    , ("-",  "sub")
+    , ("*",  "mul")
+    , ("/",  "div")]
+
+binopShorts :: [Short]
+binopShorts = map fst binopTableInit
+
+binopTable :: M.Map Short O.Operator
+binopTable = foldr (\(x, y) m -> M.insert x y m)  M.empty binopTableInit
+
+binopShort :: Parser O.Operator
+binopShort = do
+  short <- choice (map string binopShorts)
+  case (M.lookup short binopTable) of
+    Just op -> return op
+    _ -> parserZero <?> "a binary operator"
+
 singleLineInstr :: Parser A.Instr
-singleLineInstr = line $
-  eR3 <$> opcode <*> spc reg <*> spc reg <*> spc reg
-  <|> eR2 <$> opcode <*> spc reg <*> spc reg
-  <|> eR1 <$> opcode <*> spc reg
-  <|> eR0 <$> opcode
-  <|> eR1LIT <$> opcode <*> spc reg <* spc (string "lit") <*> spc literal
+singleLineInstr = (line $
+  binop
+  <|> try (eR3 <$> opcode <*> spc reg <*> spc reg <*> spc reg)
+  <|> try (eR2 <$> opcode <*> spc reg <*> spc reg)
+  <|> try (eR1LIT <$> opcode <*> spc reg <* spc (string "lit")) <*> spc literal
+  <|> try (eR1 <$> opcode <*> spc reg)
+  <|> try (eR0 <$> opcode))
+  <?> "a single line assembly instruction"
+  where binop = do
+          r1 <- reg
+          spc (string ":=")
+          r2 <- spc reg
+          op <- spc binopShort
+          r3 <- spc reg
+          return $ eR3 op r1 r2 r3
+
 
 loadFunc arity body x = A.LoadFunc x arity body
 
 instruction :: Parser A.Instr
-instruction = singleLineInstr
-            <|> loadFunBegin <*> manyTill instruction loadFunEnd
+instruction = (try singleLineInstr
+            <|> try (loadFunBegin <*> manyTill instruction loadFunEnd))
+            <?> "an assembly instruction"
             where loadFunBegin = line $ do
                     string ".loadfunc"
                     arity <- spc nat
