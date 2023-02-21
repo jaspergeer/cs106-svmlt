68f161731682959fbece82843e55271b416e6184
ahuoguo <ahuoguo@gmail.com>
2023-02-20 20:12:25 -0500

    fix let (in KNF, let only binds one local var)

--- src/huft/src/KNF.hs
+++ src/huft/src/KNF.hs
@@ -16,7 +16,7 @@ type Name = String
 data Exp a = Literal Literal
            | Name a
            | If a (Exp a) (Exp a)
-           | Let a [(a, Exp a)] (Exp a)
+           | Let a (Exp a) (Exp a)
            | Seq (Exp a) (Exp a)
            | Assign a (Exp a)
            | While a (Exp a) (Exp a)
