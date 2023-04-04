2199a3d3beadd4a90b6bdda01f044ec68d5a4e60
Jasper Geer <jasper.geer@gmail.com>
2023-04-03 21:21:05 -0400

    remove instrumentation

--- src/svm/vmrun.c
+++ src/svm/vmrun.c
@@ -185,7 +185,6 @@ void vmrun(VMState vm, struct VMFunction* fun) {
       {      
         uint8_t funreg = uX(instr);
         uint8_t lastarg = uY(instr);
-        print("r%d = %v\n",uX(instr) + (reg0 - vm->registers),RX);
         if (isNil(RX)) {
           runerror(vm, "Tried to call nil; maybe global '%s' is not defined?",
                    lastglobalset(vm, funreg, fun, stream_ptr));
@@ -211,7 +210,6 @@ void vmrun(VMState vm, struct VMFunction* fun) {
       break;
     case GetGlobal:
       RX = globals[uYZ(instr)];
-      print("r%d = %v\n",uX(instr) + (reg0 - vm->registers),RX);
       break;
     case SetGlobal:
       globals[uYZ(instr)] = RX;
