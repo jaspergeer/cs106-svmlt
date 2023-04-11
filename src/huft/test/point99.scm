477f9e4f41a2ee3bae718fdb6b8622813eefc716
JasperGeer <jasper.geer@gmail.com>
2023-04-10 21:36:20 -0400

    add point99 test

---
+++ src/huft/test/point99.scm
@@ -0,0 +1,9 @@
+;; when the bindAnyReg policy is used, y aliases x and the function will
+;; always return 99, rather than the value the caller passes.
+
+(define f (v)
+  (let ([x v])
+    (let ([y x])
+      (begin (set y 99) x))))
+
+(check-expect (f 100) 100)
